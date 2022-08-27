6dc034983ab47ce291caeaa749bcfa2e066c0a21
Joseph Pham <josephpham751@gmail.com>
2022-08-27 11:42:06 -0400

    ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,40 +2,39 @@
 
 __author__ = "730560264"
 
-five_character_word: str = input("Enter a 5-character word: ")
-if len(five_character_word) != 5:
-    print("Error: Word must contain 5 letters.")
-    quit()
-single_character: str = input("Enter a single character: ")
+five_character = input("Enter a 5-character word: ")
+if len(five_character) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+single_character = input("Enter a single character: ")
 if len(single_character) != 1:
-    print("Error: Character must be a single character.")
-    quit()
+    print("Error: Character must be a single character")
+    exit()
+print("Searching for " + single_character + " in " + five_character)
 
-print("Searching for " + single_character + " in " + five_character_word)
+matches = 0
 
-count = 0
-plural = ""
+if single_character == five_character[0]:
+    print(single_character + "found at index 0")
+    matches = matches + 1
+if single_character == five_character[1]:
+    print(single_character + "found at index 1")
+    matches = matches + 1
+if single_character == five_character[2]:
+    print(single_character + "found at index 2")
+    matches = matches + 1
+if single_character == five_character[3]:
+    print(single_character + "found at index 3")
+    matches = matches + 1
+if single_character == five_character[4]:
+    print(single_character + "found at index 4")
+    matches = matches + 1
 
-if five_character_word[0] == single_character:
-    print(single_character + " found at index 0")
-    count = count + 1 
-if five_character_word[1] == single_character:
-    print(single_character + " found at index 1")
-    count = count + 1 
-if five_character_word[2] == single_character:
-    print(single_character + " found at index 2")
-    count = count + 1 
-if five_character_word[3] == single_character:
-    print(single_character + " found at index 3")
-    count = count + 1 
-if five_character_word[4] == single_character:
-    print(single_character + " found at index 4")
-    count = count + 1 
-
-if count > 1:
-    plural: str = "s"
-    print(str(count) + " instance" + plural + " of " + single_character + " found in " + five_character_word)
+if matches != 0 and matches > 1:
+    print(str(matches) + " instances of " + single_character + " found in " + five_character)
+elif matches == 1:
+    print(str(matches) + " instance of " + single_character + " found in " + five_character)
 else:
-    print("No instances of " + single_character + " found in " + five_character_word)
+    print("No instances of " + single_character + " found in " + five_character)
 
 exit()
