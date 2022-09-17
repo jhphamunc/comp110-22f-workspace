3cfeff039d6f4187fcceda53a56e3a727d7bdd6a
Joseph Pham <josephpham751@gmail.com>
2022-09-17 00:09:02 -0400

    revised

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,10 +1,12 @@
-""" This is ex04 Utils."""
+"""This is ex04 Utils."""
 
 __author__ = "730560264"
 
 
 def all(list: int, integer: int) -> bool:
-    """ This is the all function which makes sure all the variables in a list are the same as the integer. """
+    """This is the all function which makes sure all the variables in a list are the same as the integer."""
+    if len(list) == 0:
+        raise ValueError("max() arg is an empty list")
     i = 0
     while i < len(list):
         if list[i] == integer:
@@ -25,19 +27,18 @@ def max(input: list[int]) -> int:
         x = 0
         while c < len(input) - 1 and i != len(input) and x != len(input) + 1:
             if input[i] >= input[x]:
-                x += 1
+                x = i + 1
             else:
-                i += 1
+                i = x+ 1
             c += 1
-        if input[i] > input[x]:        
+        if input[i] > input[x]:
             return input[i]
         else:
             return input[x]
 
 
 def is_equal(list1: list[int], list2: list[int]) -> bool:
-    """ This is the is_equal function which compares two lists to make sure all indexes are the same between the two. """
-    assert len(list1) == len(list2)
+    """This is the is_equal function which compares two lists to make sure all indexes are the same between the two."""
     i = 0
     while i < len(list1):
         if list1[i] == list2[i]:
