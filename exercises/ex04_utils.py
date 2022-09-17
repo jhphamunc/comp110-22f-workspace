4d598b01a58589fc8f9779149e840e3b7f0c5ce6
Joseph Pham <josephpham751@gmail.com>
2022-09-16 23:40:11 -0400

    draft

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,21 +1,48 @@
-""" EX04 Utils"""
+""" This is ex04 Utils."""
 
 __author__ = "730560264"
 
-import numbers
-
 
 def all(list: int, integer: int) -> bool:
+    """ This is the all function which makes sure all the variables in a list are the same as the integer. """
     i = 0
-    print(list)
-    print(integer)
-    while i < 3:
+    while i < len(list):
         if list[i] == integer:
             i += 1
-        else: 
+        else:
             return False
-            
+    else:
+        return True
 
-sa: list[int] = [1,1,1]
-all(sa, 1)
 
+def max(input: list[int]) -> int:
+    """ This is the max function which looks at all the indexes in a list and finds the max. """
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty list")
+    else:
+        c = 0
+        i = 0
+        x = 0
+        while c < len(input) - 1 and i != len(input) and x != len(input) + 1:
+            if input[i] >= input[x]:
+                x += 1
+            else:
+                i += 1
+            c += 1
+        if input[i] > input[x]:        
+            return input[i]
+        else:
+            return input[x]
+
+
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """ This is the is_equal function which compares two lists to make sure all indexes are the same between the two. """
+    assert len(list1) == len(list2)
+    i = 0
+    while i < len(list1):
+        if list1[i] == list2[i]:
+            i += 1
+        else:
+            return False
+    else: 
+        return True
