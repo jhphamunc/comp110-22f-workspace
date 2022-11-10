e41b918eb33c929e680c9b858982d0b80eade2db
Joseph Pham <josephpham751@gmail.com>
2022-11-10 11:31:15 -0500

    yes

--- lessons/testing_lab.py
+++ lessons/testing_lab.py
@@ -1,4 +1,25 @@
-number: list[int] = list()
-number.append(1)
-number.append(3)
-print(number[3])
+class Light:
+
+    on: bool
+    
+    def __init__(self):
+        self.on = False
+    
+    def __repr__(self) -> str:
+        if self.on:
+            return "light is on."
+        else:
+            return "light is off."
+
+    def _bool__(self) -> bool:
+        return self.on
+    
+    def switch(self) -> None:
+        self.on = not self.on
+
+
+if __name__ == "__main__":
+    a: Light = Light()
+    a.switch()
+    if bool(a):
+        print(a)
